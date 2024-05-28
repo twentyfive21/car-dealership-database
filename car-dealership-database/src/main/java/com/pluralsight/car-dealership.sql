23af744e6e23281744724bbbcbe5990ebebb3d35
twentyfive21 <tina.winnn@gmail.com>
2024-05-27 23:30:59 -0400

    setting up workshop files

---
+++ car-dealership-database/src/main/java/com/pluralsight/car-dealership.sql
@@ -0,0 +1,14 @@
+-- DROP to start with a new DB
+DROP DATABASE dealership;
+-- CREATE NEW TABLE
+CREATE DATABASE dealership;
+
+-- CREATE dealership table
+
+-- CREATE vehicles table
+
+-- CREATE inventory table
+
+-- CREATE sales contract table
+
+-- CREATE lease contracts table
